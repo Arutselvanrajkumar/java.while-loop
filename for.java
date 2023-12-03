a93e1038ec71e970915b4d01d329736e0a4eecb0
Arutselvanrajkumar <145752344+Arutselvanrajkumar@users.noreply.github.com>
2023-12-03 14:46:45 +0530

    Create for.java

---
+++ for.java
@@ -0,0 +1,11 @@
+public class Main{
+ public static void main(String[]agrs){
+ for(int i=4; i <= 65; i=i+4){
+ System.out.println("age"+i);
+ 
+ for(int a=6; a<15; a++){
+ System.out.println("numbers"+ a);
+ }
+ }
+ }
+ }
